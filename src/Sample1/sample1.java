83b8cdc31c11f3a7a82b1d9f5fa1e181712cb3c5
vishal <vishal@LAPTOP-L8IAANVU>
2022-10-03 17:30:02 +0530

    First Commit

---
+++ src/Sample1/sample1.java
@@ -0,0 +1,20 @@
+package Sample1;
+
+public class sample1 
+{
+	 public static void main(String[] args)
+		{
+			// type number from 1 to 5
+			
+			// use for loop 
+			// Formula   for(starting number; last number; increase or decrease) {}
+		
+				for (int i=1; i<=5; i++) 
+				{
+					System.out.println(i);
+					
+				}
+		
+		}
+
+}
